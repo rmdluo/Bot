b893ed5628edea4a3d3663c47deb6bfe885266e8
rmdluo <40529960+rmdluo@users.noreply.github.com>
2021-11-11 01:49:24 -0500

    Update TABot.py

--- TABot.py
+++ TABot.py
@@ -60,7 +60,7 @@ class MyClient(discord.Client):
         self.trader_signals.start()
 
     async def on_ready(self):
-        await self.change_presence(activity=discord.Activity(name='***Not yet implemented! !help', type="watching"))
+        await self.change_presence(activity=discord.Activity(name='***Not yet implemented! !help', type=discord.ActivityType.watching))
         print(f'We have logged in as {self.user} (ID: {self.user.id})')
 
     #****MACDTrader functions****
