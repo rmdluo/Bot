5d56f0d292fc08e39dc143731ae2b78b81b216dd
rmdluo <40529960+rmdluo@users.noreply.github.com>
2021-11-10 23:59:06 -0500

    Update WeatherBot.py

--- WeatherBot.py
+++ WeatherBot.py
@@ -80,7 +80,7 @@ class WeatherBot():
             + "Temperature: " + str(weather_json['main']['temp']) + '\u00b0'+ " F\n" \
             + "Feels like: " + str(weather_json['main']['feels_like']) + '\u00b0'+ " F\n" \
             + "Humidity: " + str(weather_json['main']['humidity']) + "%\n" \
-            + "Cloudiness/rain: " + weather_json['weather'][0]['main'] + " - " + weather_json['weather'][0]['description'] + "\n" \
+            + "Weather conditions: " + weather_json['weather'][0]['description'] + "\n" \
             + "Wind: " + str(weather_json['wind']['speed']) + " MPH, " + str(weather_json['wind']['deg']) + '\u00b0' + "\n" \
             + "Wind gusts: " + str(weather_json['wind']['gust']) + "MPH"
         
