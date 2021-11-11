572683b700445be0f5eb9c6e1fdd4dff9e38c154
rmdluo <40529960+rmdluo@users.noreply.github.com>
2021-11-10 23:33:45 -0500

    Update WeatherBot.py

--- WeatherBot.py
+++ WeatherBot.py
@@ -76,13 +76,16 @@ class WeatherBot():
         return format_weather_json(requests.get("https://api.openweathermap.org/data/2.5/weather", params = payload))
     
     def format_weather_json(weather_json):
-        str = "__Weather in " + weather_json["name"] + "__\n" \
+        str = weather_json["name"]
+        """
+            "__Weather in " + weather_json["name"] + "__\n" \
             + "Temperature: " + str(weather_json["main"]["temp"]) + '\u00b0'+ " F\n" \
             + "Feels like: " + str(weather_json["main"]["feels_like"]) + '\u00b0'+ " F\n" \
             + "Humidity: " + str(weather_json["main"]["humidity"]) + "%\n" \
             + "Cloudiness/rain: " + weather_json["weather"]["main"] + " - " + weather_json["weather"]["description"] + "\n" \
             + "Wind: " + weather_json["wind"]["speed"] + " MPH, " weather_json["wind"]["deg"] + '\u00b0\ + "\n" \
             + "Wind gusts: " + weather_json["wind"]["gust"] + "MPH"
+        """
         
         return str
         
