fefc04f55467e5e52cffdfbe7e35753e8bd49680
rmdluo <40529960+rmdluo@users.noreply.github.com>
2022-07-03 06:53:38 -0400

    bug fix

--- ListBot.py
+++ ListBot.py
@@ -26,9 +26,6 @@ class List:
     def remove_item(self, item_index):
         return self.items.pop(item_index)
 
-    def remove_item(self, item):
-        return self.items.remove(item)
-
     def get_item(self, item_index):
         return self.items[item_index]
 
